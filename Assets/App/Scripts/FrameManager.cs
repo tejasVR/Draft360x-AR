23ec07f252c7eeb50d3134367944540112567299
tejasVR <tejasshroff24@gmail.com>
2020-05-20 21:30:29 -0400

    Adding running cat prefabs

---
+++ Assets/App/Scripts/FrameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Draft360
+{
+    public class FrameManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
